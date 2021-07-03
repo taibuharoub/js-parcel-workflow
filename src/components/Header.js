cbed89f46e2ecfc41fcdb7f51e63620061fb5942
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-03 18:01:27 +0300

    Add user and header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    const template = `
+        <header>My Parcel App</header>
+        <p>This is a boilerplate for a simple JS workflow
+        using parcel</p>
+    `
+
+    return template;
+}
+
+export default Header;
