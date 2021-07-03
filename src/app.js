10d94bf2c854ed28629c210e4f6a0e541338ba83
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-03 18:02:40 +0300

    Final files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+import Header from "./components/Header";
+import User from "./components/User";
+
+//directky import the scss file
+import "./scss/app.scss"
+
+const app = async () => {
+    document.getElementById("header").innerHTML = Header();
+    document.getElementById("user").innerHTML = await User();
+}
+
+//Init app
+app();
+
 console.log("It works!");
